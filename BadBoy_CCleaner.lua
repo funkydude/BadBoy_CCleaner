d8f06149e065f4137e07c61a4daaa8630f8b4102
funkydude <funkydude@users.noreply.github.com>
2009-05-19 16:42:32 +0000

    Initial commit

---
+++ BadBoy_CCleaner.lua
@@ -0,0 +1,25 @@
+local ipairs = _G.ipairs
+local fnd = _G.string.find
+local lower = _G.string.lower
+
+local triggers = {
+	"%[.*%].*rectum",
+	"rectum.*%[.*%]",
+	"%[.*%].*anal",
+	"anal.*%[.*%]",
+}
+
+local savedID, result = 0, nil
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_,_,msg,_,_,_,_,_,chanid,_,_,_,id)
+	if id == savedID then return result else savedID = id end --incase a message is sent more than once
+	if chanid == 0 then result = nil return end --Only scan official custom channels (gen/trade)
+	msg = lower(msg) --lower all text
+	for k, v in ipairs(triggers) do
+		if fnd(msg, v) then
+			result = true
+			return true --found a trigger, filter
+		end
+	end
+	result = nil
+end)
+
