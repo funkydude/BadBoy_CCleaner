fb708504cd1d08b81fac352cd18b5b4f088a0713
funkydude <funkydude@users.noreply.github.com>
2009-06-09 15:41:26 +0000

    Better macro detection and better spam filter

--- BadBoy_CCleaner.lua
+++ BadBoy_CCleaner.lua
@@ -1,6 +1,7 @@
 local ipairs = _G.ipairs
 local fnd = _G.string.find
 local lower = _G.string.lower
+local rep = _G.string.gsub
 
 local triggers = {
 	"%[.*%].*rectum",
@@ -15,12 +16,14 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_,_,msg,_,_,_,_,_,c
 	if chanid == 0 then result = nil return end --Only scan official custom channels (gen/trade)
 	if not _G.CanComplainChat(id) then result = nil return end --Don't filter ourself
 	msg = lower(msg) --lower all text
+	msg = rep(msg, " ", "") --Remove spaces
 	for k, v in ipairs(triggers) do
 		if fnd(msg, v) then
 			result = true
 			return true --found a trigger, filter
 		end
 	end
+	msg = rep(msg, "%A", "") --Remove non-letters after blacklist scan, need brackets for that
 	if msg == oldmsg then
 		result = true
 		return true
